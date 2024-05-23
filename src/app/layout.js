3566bce357d0ff205a00b262b5a8922cb6b0eb02
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-23 11:11:52 +0530

    rh-comrade: bootstrap integrated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,11 @@
+
 import { Inter } from "next/font/google";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'jquery/dist/jquery.js'
+// import 'bootstrap/dist/js/bootstrap.bundle.js'
 import "./globals.css";
+import { Header } from "@/header";
+import { Footer } from "@/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,9 +15,14 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header/>
+      {children}
+      <Footer/>
+      </body>
     </html>
   );
 }
---
+++ src/header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '@/components/navbar'
+import styles from './header.module.css'
+const Header = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Header
